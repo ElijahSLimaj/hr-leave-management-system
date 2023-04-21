ede3549dbe275a148417b5b48c8811fc5b583d7d
Elijah <limaj.sulejman@gmail.com>
2023-04-21 05:14:01 +0200

    Work on infrastructure layer

---
+++ HrLeaveManagementSystem/Src/Infrastructure/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,9 @@
+namespace HrLeaveManagementSystem.Src.Infrastructure.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
