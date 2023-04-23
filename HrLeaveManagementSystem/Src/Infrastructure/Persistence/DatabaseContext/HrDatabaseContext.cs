0a9d57fafd27d30bdb48a5b0b9a63450fe17efd3
Elijah <limaj.sulejman@gmail.com>
2023-04-23 07:51:20 +0200

    make db entries nullable

--- HrLeaveManagementSystem/Src/Infrastructure/Persistence/DatabaseContext/HrDatabaseContext.cs
+++ HrLeaveManagementSystem/Src/Infrastructure/Persistence/DatabaseContext/HrDatabaseContext.cs
@@ -11,9 +11,9 @@ public class HrDatabaseContext : DbContext
 
     }
 
-    public DbSet<LeaveType> LeaveTypes { get; set; }
-    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
-    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+    public DbSet<LeaveType>? LeaveTypes { get; set; }
+    public DbSet<LeaveAllocation>? LeaveAllocations { get; set; }
+    public DbSet<LeaveRequest>? LeaveRequests { get; set; }
 
 
 
