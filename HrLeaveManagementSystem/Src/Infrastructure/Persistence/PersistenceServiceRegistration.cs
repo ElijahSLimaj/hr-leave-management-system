95b1bd03d6a83f0a88a702b47f66da2ecc421594
Elijah <limaj.sulejman@gmail.com>
2023-04-23 17:56:24 +0200

    add di for generic repo

--- HrLeaveManagementSystem/Src/Infrastructure/Persistence/PersistenceServiceRegistration.cs
+++ HrLeaveManagementSystem/Src/Infrastructure/Persistence/PersistenceServiceRegistration.cs
@@ -1,4 +1,6 @@
+using HrLeaveManagementSystem.Src.Core.Application.Contracts.Persistence;
 using HrLeaveManagementSystem.Src.Infrastructure.Persistence.DatabaseContext;
+using HrLeaveManagementSystem.Src.Infrastructure.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 namespace HrLeaveManagementSystem.Src.Infrastructure.Persistence;
@@ -8,9 +10,12 @@ public static class PersistenceServiceRegistration
     public static IServiceCollection AddPersistenceServices(
         this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<HrDatabaseContext>(options => {
+        services.AddDbContext<HrDatabaseContext>(options =>
+        {
             options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConfigurationString"));
-         });
+        });
+
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
         return services;
     }
 }
